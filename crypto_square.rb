4578977df730fdc4622a7dd21dd5748415d57bb7
Tony DiNitto <think41c@gmail.com>
2015-05-11 20:43:49 -0400

    Cleaned useless expressions.

--- crypto_square.rb
+++ crypto_square.rb
@@ -11,7 +11,6 @@ class Crypto
   end
 
   def normalize_ciphertext
-    # ciphertext
   end
 
   def ciphertext
@@ -24,15 +23,11 @@ class Crypto
       col_counter = 0 
       result = []
       until col_counter == size
-        p "Plain result -> #{@plain_result}"
-        p "col_counter -> #{col_counter}"
-        p "row_counter -> #{row_counter}"
-        p "result -> #{result}"
         result << @plain_result[col_counter][row_counter]
         col_counter += 1   
       end
       row_counter += 1
-      puts total_result
+      # puts total_result
       total_result << result.join.scan(/.{1,#{size}}/)
     end
     total_result.flatten.join
@@ -44,14 +39,12 @@ class Crypto
     row_characters = size
     plain_rows = []
     
-      until col_counter == row_characters * row_characters
-        plain_rows << @result[col_counter]
-        col_counter += 1 
-      end
-      plain_rows
-    
+    until col_counter == row_characters * row_characters
+      plain_rows << @result[col_counter]
+      col_counter += 1 
+    end
+    plain_rows
     @plain_result = plain_rows.join.scan(/.{1,#{row_characters}}/)
-    p @plain_result
   end
 
   def size
@@ -64,8 +57,6 @@ class Crypto
     square
   end
 end
-# Errors occur when text is the following lengths:
-# 5, 6 10, 11 12, 17 , 18, 19 20, 
-a = Crypto.new('0fsdafgdf0000')
+
+a = Crypto.new('Vampires are people tooA!')
 p a.ciphertext
-# p a.normalize_ciphertext
